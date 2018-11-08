6cea0fd03f8a2f0397ff62149371f037d648c357
Hoodathunk <naperken@gmail.com>
2018-11-08 17:16:10 -0600

    Updated default personality scripts.

--- CargoMonitor/CargoEvent.cs
+++ CargoMonitor/CargoEvent.cs
@@ -7,8 +7,8 @@ namespace EddiCargoMonitor
 {
     public class CargoEvent : Event
     {
-        public const string NAME = "Cargo inventory";
-        public const string DESCRIPTION = "Triggered when you obtain an inventory of your cargo";
+        public const string NAME = "Cargo";
+        public const string DESCRIPTION = "Triggered when a vehicle cargo inventory is updated";
         public const string SAMPLE = "{ \"timestamp\":\"2018-10-31T03:39:10Z\", \"event\":\"Cargo\", \"Count\":32, \"Inventory\":[ { \"Name\":\"hydrogenfuel\", \"Name_Localised\":\"Hydrogen Fuel\", \"Count\":1, \"Stolen\":0 }, { \"Name\":\"biowaste\", \"MissionID\":426282789, \"Count\":30, \"Stolen\":0 }, { \"Name\":\"animalmeat\", \"Name_Localised\":\"Animal Meat\", \"Count\":1, \"Stolen\":0 } ] }";
 
         public static Dictionary<string, string> VARIABLES = new Dictionary<string, string>();
